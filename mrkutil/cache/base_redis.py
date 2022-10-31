2cb4f79707aeb1b666a6966568b826ca22be2a91
Mladen <mladjone@gmail.com>
2022-10-31 11:19:46 +0100

    Redis base improved

--- mrkutil/cache/base_redis.py
+++ mrkutil/cache/base_redis.py
@@ -31,11 +31,25 @@ class RedisBase(object):
     def _delData(self, key):
         return self.server.delete(key)
 
+    def _getMultiple(self, keys):
+        return self.server.mget(keys)
+
     def get(self, key):
         data = self._getData("{}_{}".format(self._key, key))
         try:
             if data:
                 data = json.loads(data)
+        except Exception as e:
+            logger.warn("Stored data is not dictionary. Exception: {}".format(str(e)))
+            pass
+        return data
+
+    def get_multiple(self, keys):
+        keys = [f"{self._key}_{x}" for x in keys]
+        data = self._getMultiple(keys)
+        try:
+            if data:
+                data = [json.loads(x) for x in data]
         except Exception as e:
             logger.warn("Stored data is not dictionary. Exception: {}".format(str(e)))
             pass
@@ -51,7 +65,8 @@ class RedisBase(object):
         return self._delData("{}_{}".format(self._key, key))
 
     def search(self, pattern: str):
-        return self.server.keys(pattern=pattern)
+        keys = self.server.keys(pattern=self._key+"_"+pattern)
+        return [(x.decode("utf-8")).strip(self._key) for x in keys]
 
     def delete_keys(self, pattern: str):
         keys = self.search(pattern)
